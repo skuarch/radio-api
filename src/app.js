478747903855a722c336a9ca9d03f28267623b0c
skuarch <skuarch@yahoo.com.mx>
2021-08-27 08:25:59 -0500

    Uncomment

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { env } from './config/environments/index.js';
 import { loadControllers } from './config/loaders/controller.loader.js';
 import { authenticateDb, syncDb } from './config/loaders/db.loader.js';
 import { shutdownHook } from './config/loaders/shutdown.loader.js';
-// import { cert } from './config/loaders/certificate.loader.js';
+import { cert } from './config/loaders/certificate.loader.js';
 import { loadStaticContent } from './config/loaders/static.loader.js';
 import http from 'http';
 import https from 'https';
@@ -19,7 +19,7 @@ loadControllers(app);
 loadStaticContent(app);
 
 export const httpServer = http.createServer(app);
-export const httpsServer = https.createServer(app);
+export const httpsServer = https.createServer(cert, app);
 
 httpServer.listen(env.httpServerPort, () => {
   console.log(`HTTP Server running on port ${env.httpServerPort}`);
